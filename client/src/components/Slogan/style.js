8e5047127aa942191746a74830332acb51434fb1
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-19 23:07:53 -0300

    fixed slogan overflow add rectangle translateX

--- client/src/components/Slogan/style.js
+++ client/src/components/Slogan/style.js
@@ -4,29 +4,29 @@ export const Container = styled.div`
   .box {
     padding: 2.4rem;
     position: relative;
-    left: 52%;
-    transform: translateX(-50%);
+    /* left: 2%; */
   }
-
+  
   .pngegg {
     position: absolute;
     width: 19.0rem;
     height: 14.9rem;
-    left: -.2rem;
+    left: .2rem;
     bottom: 2.75rem;
   }
-
+  
   .rectangle {
     margin-top: 3.6rem;
     width: 100%;
     height: 9.3rem;
+    transform: translateX(2%);
   }
 
   .slogan {
     width: 50%;
     position: absolute;
     bottom: 4.8rem;
-    right: 1.6rem;
+    right: 1.2rem;
   }
   
   .slogan h2 {
