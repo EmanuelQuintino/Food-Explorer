403844e8f9d81a6f4e30ad7c9d2aeb9ccf3f6463
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-23 15:44:35 -0300

    add read client controller

--- server/src/controllers/clientControllers.ts
+++ server/src/controllers/clientControllers.ts
@@ -1,7 +1,22 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { prisma } from "../database";
+import { log } from "console";
 
 export const clientControllers = {
-  read: async (req: Request, res: Response) => {
-    res.status(200).json({message: "OK"});
+  read: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.query;      
+      if (id) {
+        const client = await prisma.clients.findUnique({where: { id: String(id)}});
+        if (!client) return res.status(400).json('Cliente não encontrado');
+        return res.status(200).json({client});
+      } else {
+        const clients = await prisma.clients.findMany();
+        return res.status(200).json({clients});
+      }
+    } catch (error) {
+      // if (error.code == "P2021") return res.status(500).json("Tabela não encontrada");
+      next(error);
+    }
   }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import { routes } from "./routes";
 import { prisma } from "./database";
-import { log } from "console";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
-
 app.use(routes);
-
 app.listen(3000, () => console.log("Server is running on port 3000"));
+
+app.use((req, res, next: NextFunction) => {
+  const error = new Error('Page not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(error.status || 500);
+  res.json({error: error.message});
+});
 
 prisma.$connect()
   .then(() => console.log("Database is connected.."))
