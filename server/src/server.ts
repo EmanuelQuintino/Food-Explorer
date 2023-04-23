3250fae16a82d140bebad83a7d3a493717398533
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-23 17:47:59 -0300

    refactor middleware error

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,8 +15,7 @@ app.use((req, res, next: NextFunction) => {
 });
 
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(error.status || 500);
-  res.json({error: error.message});
+  return res.status(error.status || 500).json({error: error.message});
 });
 
 prisma.$connect()
