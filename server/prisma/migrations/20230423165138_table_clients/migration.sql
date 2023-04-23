dc46de83f4b1086f0379802f1b16e59c23969cef
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-23 14:07:59 -0300

    add prisma database connection

---
+++ server/prisma/migrations/20230423165138_table_clients/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_email_key" ON "clients"("email");
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,14 @@
-import express from "express"
+import express from "express";
 import { routes } from "./routes";
+import { prisma } from "./database";
+import { log } from "console";
 
 const app = express();
 
 app.use(routes);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
+
+prisma.$connect()
+  .then(() => console.log("Database is connected.."))
+  .catch((error) => console.error(error));
