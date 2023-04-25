0b48ed55680567c76f7310c468d6c0d88b5d1c2a
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-25 15:10:40 -0300

    add log error status 500

--- server/src/errors/appError.ts
+++ server/src/errors/appError.ts
@@ -7,8 +7,11 @@ interface AppError {
 }
 
 export function appError(error: AppError, req: Request, res: Response, next: NextFunction) {
+  console.error(error);
+
   if (error instanceof ZodError) {
-    return res.status(error.status || 400).json({error: JSON.parse(error.message)[0].message});
+    return res.status(error.status || 500).json({error: JSON.parse(error.message)[0].message});
   };
+  
   return res.status(error.status || 500).json({error: error.message});
 }
