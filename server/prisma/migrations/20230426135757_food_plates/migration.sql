db1c3a33207fe5504126fbc9f4488abbb4126bbc
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-26 11:30:15 -0300

    add model food plates

---
+++ server/prisma/migrations/20230426135757_food_plates/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "food_plates" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "image" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
