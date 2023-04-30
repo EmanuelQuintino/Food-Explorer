3a1198f9df8248f8e499c34ecb26446adf5b905a
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-30 16:48:22 -0300

    add main types

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,7 +6,7 @@ import { GlobalStyles } from './styles/global'
 import { SystemProvider } from './context/system'
 import { Routes } from "./routes"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <SystemProvider>
       <ThemeProvider theme={Theme}>
