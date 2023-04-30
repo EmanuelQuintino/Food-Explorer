2706decc11d2a03d2ef6985ffadbbec02130ab4c
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-30 16:06:18 -0300

    add global style types

---
+++ client/src/@types/globalStyle.d.ts
@@ -0,0 +1,34 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      background: string,
+      dark100: string,
+      dark1000: string,
+      dark200: string,
+      dark300: string,
+      dark400: string,
+      dark500: string,
+      dark600: string,
+      dark700: string,
+      dark800: string,
+      dark900: string,
+      gradient100: string,
+      gradient200: string,
+      light100: string,
+      light200: string,
+      light300: string,
+      light400: string,
+      light500: string,
+      light600: string,
+      light700: string,
+      carrot: string,
+      tomato100: string,
+      tomato200: string,
+      tomato300: string,
+      tomato400: string,
+      cake200: string,
+    }
+  }
+}
