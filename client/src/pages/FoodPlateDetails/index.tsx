bd232f37e3e02976b46b9d4e5ebdab9f440e0299
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-05-05 13:25:49 -0300

    add button back navigate

--- client/src/pages/FoodPlateDetails/index.tsx
+++ client/src/pages/FoodPlateDetails/index.tsx
@@ -1,15 +1,18 @@
 import { Container } from "./style"
 import { useSystem } from "../../hooks/system"
 import { useParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export function FoodPlateDetails() {
   const { menuActive } = useSystem();
   const params = useParams();
-  
+  const navigate = useNavigate();
+
   return (
     <Container>
       {!menuActive &&
-        <>          
+        <>
+          <button onClick={() => navigate(-1)}>&lt;Voltar</button>      
           <p>Details: {params.id}</p>
         </>
       }
