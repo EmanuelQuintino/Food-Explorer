d04d4eec1727e2b86540aca2ed4708582bcb7398
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-05-22 19:41:03 -0300

    add migration create tables

--- server/prisma/migrations/20230427151123_table_orders/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "orders" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "status" TEXT NOT NULL DEFAULT "Pendente",
-    "users_id" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "orders_users_id_fkey" FOREIGN KEY ("users_id") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
--- server/prisma/migrations/20230428220909_table_favorites/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "favorites" (
-    "user_id" TEXT NOT NULL,
-    "plate_id" TEXT NOT NULL,
-
-    PRIMARY KEY ("user_id", "plate_id"),
-    CONSTRAINT "favorites_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "favorites_plate_id_fkey" FOREIGN KEY ("plate_id") REFERENCES "plates" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
