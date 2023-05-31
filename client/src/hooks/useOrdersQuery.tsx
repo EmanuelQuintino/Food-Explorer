2c89c7dcd2c0651c23429b316727ec27e3064e86
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-05-31 13:05:04 -0300

    add hook useOrdersQuery

---
+++ client/src/hooks/useOrdersQuery.tsx
@@ -0,0 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
+import { API } from "../services/api";
+
+type OrderPlatesTypes = {
+  order_id: string;
+  plate_id: string;
+  amount: number;
+  price: number;
+}
+
+type UserOrdersTypes = {
+  data: {
+    id: string;
+    status: string;
+    code: number;
+    user_id: string;
+    order_plates: OrderPlatesTypes[]
+  }[];
+}
+
+export const useOrdersQuery = () => {
+  const query = useQuery({
+    queryKey: ['userOrders'],
+    queryFn: async () => await API.get("/orders") as UserOrdersTypes
+  });
+
+  const refetchPlateQuery = async () => await query.refetch();
+
+  return {
+    ...query,
+    data: query.data?.data,
+    refetchPlateQuery
+  }
+} 
