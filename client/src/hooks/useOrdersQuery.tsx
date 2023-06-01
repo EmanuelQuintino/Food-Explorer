c3a294568ee0a3d314c04e2dfeaf210d1957f8be
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-01 09:20:54 -0300

    add auth.isAdmin logic uri index to useOrdersQuery

--- client/src/hooks/useOrdersQuery.tsx
+++ client/src/hooks/useOrdersQuery.tsx
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { API } from "../services/api";
+import { useAuth } from "./useAuth";
 
 type OrderPlatesTypes = {
   order_id: string;
   plate_id: string;
   amount: number;
   price: number;
-}
+};
 
 type UserOrdersTypes = {
   data: {
@@ -17,12 +18,14 @@ type UserOrdersTypes = {
     order_plates: OrderPlatesTypes[]
     created_at: string;
   }[];
-}
+};
 
 export const useOrdersQuery = () => {
+  const { userAuth } = useAuth();
+  const URI = `/orders${userAuth.isAdmin ? "/index" : ""}`
   const query = useQuery({
     queryKey: ['userOrders'],
-    queryFn: async () => await API.get("/orders") as UserOrdersTypes
+    queryFn: async () => await API.get(URI) as UserOrdersTypes
   });
 
   const refetchOrdersQuery = async () => await query.refetch();
@@ -31,5 +34,5 @@ export const useOrdersQuery = () => {
     ...query,
     data: query.data?.data,
     refetchOrdersQuery
-  }
-} 
+  };
+};
