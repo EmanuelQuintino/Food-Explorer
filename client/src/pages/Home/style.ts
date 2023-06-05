6c0897108af4a502a197aa94e3827c6d71cb5afe
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-05 09:21:48 -0300

    update margin boxPlates

--- client/src/pages/Home/style.ts
+++ client/src/pages/Home/style.ts
@@ -2,22 +2,20 @@ import styled from "styled-components"
 
 export const Container = styled.section`  
   .boxPlates {
-    margin: 2.4rem 0;
+    margin: 1.2rem 0;
+    padding: 0 0 0 2.4rem;
   
     h2 {
       font-size: 1.8rem;
       font-weight: 500;
       font-family: 'Poppins', sans-serif;
-      margin: 0 0 0 2.4rem;
     }
         
     .plates {
-      width: 100%;
       display: flex;
       align-items: center;
       overflow-x: auto;
       scroll-snap-type: x mandatory;
-      margin-top: .8rem;
     }
     
     .plates::-webkit-scrollbar {
@@ -27,9 +25,18 @@ export const Container = styled.section`
 
   @media (min-width: 640px) {    
     .boxPlates {
+      width: 85%;
+      margin: 2.4rem auto;
+      
       h2 {
         font-size: 2.4rem;
       }
+    }
+  }
+
+  @media (min-width: 960px) {    
+    .boxPlates {
+      width: 80%;
     }
   }
 `;
