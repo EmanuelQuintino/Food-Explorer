40323bbd4dd15e769bcf7132fce77e1daf5c59f1
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-06 12:52:10 -0300

    remove window scroll to top

--- client/src/pages/NewPlate/index.tsx
+++ client/src/pages/NewPlate/index.tsx
@@ -33,10 +33,6 @@ export function NewPlate() {
   const { menuActive } = useSystem();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    window.scrollTo({ top: 0 });
-  }, []);
-
   const { control, register, handleSubmit, formState: { errors }, watch, setError, reset } = useForm<PlateDataTypes>();
 
   const { fields, append, remove } = useFieldArray({
