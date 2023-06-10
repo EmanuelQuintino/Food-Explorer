c65cbef9f80d691c30d85b46f71d7be82c5d754b
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-10 09:46:38 -0300

    update upload TMP_FOLDER path

--- server/src/configs/upload.ts
+++ server/src/configs/upload.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import multer from "multer";
 
-export const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+export const TMP_FOLDER = path.resolve(__dirname, "..", "tmp");
 export const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 export const MULTER = {
