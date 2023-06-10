d5a96c4c50452f0f05fb8264ab045b940975d1b0
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-10 12:09:17 -0300

    add API render baseURL

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const API = axios.create({
     // baseURL: "http://localhost:3000"
-    baseURL: "https://food-explorer-api-d8xi.onrender.com"
+    baseURL: "https://food-explorer-api-28br.onrender.com"
 });
