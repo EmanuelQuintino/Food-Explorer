0851a3fe390ba14fe65a964d25d2cb86a1812a28
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-06-19 12:26:46 -0300

    add editePlate plateData condition for render

--- src/pages/EditPlate/index.tsx
+++ src/pages/EditPlate/index.tsx
@@ -164,137 +164,141 @@ export function EditPlate() {
           {isLoading && <p><ImSpinner2 className="spinner" /></p>}
           {error && <p className="queryError">Algo deu errado!</p>}
 
-          <form onSubmit={handleSubmit(onSubmitUpdatePlate)} id="formEditPlate">
-            <div className="formPart1">
-              <section>
-                <InputFile
-                  id="uploadImagePlate"
-                  label="Imagem do prato"
-                  placeholder={inputFileName ? inputFileName : "Selecione imagem para alterá-la"}
-                  icon={UploadIcon}
-                  onChange={(event) => setInputFileName(event.target.files[0].name)}
-                  error={errors.image?.message}
-                  register={register("image", {
-                    maxLength: { value: 255, message: "Número máximo de caracteres é 255" },
-                    validate: {
-                      fileFormat: (file) => {
-                        if (!file || file.lenght === 0) return;
-                        /\.(jpg|jpeg|png|gif)$/i.test(file[0]?.name) || "Permitido somente tipo imagem"
-                      },
-                      fileCount: (file) => {
-                        if (!file || file.lenght === 0) return;
-                        file.length === 1 || "Por favor adicionar apenas uma imagem"
-                      },
-                      fileSize: (file) => {
-                        if (!file || file.lenght === 0) return;
-                        file[0]?.size <= 2 * 2 ** 20 || "O tamanho máximo permitido é de 2MB"
-                      },
-                    }
-                  })}
-                />
-              </section>
-
-              <section>
-                <Input
-                  id="name"
-                  label="Nome"
-                  type="text"
-                  value={plateDataForm.name}
-                  placeholder="Ex.: Salada Ceasar"
-                  onChange={handleInputChange}
-                  error={errors.name?.message}
-                  register={register("name", {
-                    shouldUnregister: true,
-                    required: plateDataForm.name ? false : "Campo obrigatório",
-                    pattern: { value: /^[^0-9]+$/, message: "Somente texto é permitido" },
-                    maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
-                  })}
-                />
-              </section>
-
-              <section>
-                <Select
-                  id="category"
-                  label="Categoria"
-                  value={plateDataForm.category}
-                  options={["Refeições", "Sobremesas", "Bebidas"]}
-                  onChange={handleInputChange}
-                  error={errors.category?.message}
-                  register={register("category", {
-                    required: plateDataForm.category ? false : "Campo obrigatório",
-                    pattern: { value: /^(Refeições|Sobremesas|Bebidas)$/, message: "Somente uma das opções é permitido" },
-                    maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
-                  })}
-                />
-              </section>
-            </div>
-
-            <div className="formPart2">
-              <section className="containerIngredients">
-                <label htmlFor="boxIngredients">Ingredientes</label>
-                <div id="boxIngredients">
-                  {fields?.map((ingredient, index) => (
-                    <InputList
-                      key={ingredient.id}
-                      value={ingredient.name}
-                      onClick={() => remove(index)}
+          {plateData &&
+            <>
+              <form onSubmit={handleSubmit(onSubmitUpdatePlate)} id="formEditPlate">
+                <div className="formPart1">
+                  <section>
+                    <InputFile
+                      id="uploadImagePlate"
+                      label="Imagem do prato"
+                      placeholder={inputFileName ? inputFileName : "Selecione imagem para alterá-la"}
+                      icon={UploadIcon}
+                      onChange={(event) => setInputFileName(event.target.files[0].name)}
+                      error={errors.image?.message}
+                      register={register("image", {
+                        maxLength: { value: 255, message: "Número máximo de caracteres é 255" },
+                        validate: {
+                          fileFormat: (file) => {
+                            if (!file || file.lenght === 0) return;
+                            /\.(jpg|jpeg|png|gif)$/i.test(file[0]?.name) || "Permitido somente tipo imagem"
+                          },
+                          fileCount: (file) => {
+                            if (!file || file.lenght === 0) return;
+                            file.length === 1 || "Por favor adicionar apenas uma imagem"
+                          },
+                          fileSize: (file) => {
+                            if (!file || file.lenght === 0) return;
+                            file[0]?.size <= 2 * 2 ** 20 || "O tamanho máximo permitido é de 2MB"
+                          },
+                        }
+                      })}
                     />
-                  ))}
+                  </section>
 
-                  <InputList
-                    isNew
-                    placeholder="Adicionar"
-                    value={newIngredient}
-                    onChange={(event) => setNewIngredient(event.target.value)}
-                    onClick={handleAddIngredient}
-                  />
+                  <section>
+                    <Input
+                      id="name"
+                      label="Nome"
+                      type="text"
+                      value={plateDataForm.name}
+                      placeholder="Ex.: Salada Ceasar"
+                      onChange={handleInputChange}
+                      error={errors.name?.message}
+                      register={register("name", {
+                        shouldUnregister: true,
+                        required: plateDataForm.name ? false : "Campo obrigatório",
+                        pattern: { value: /^[^0-9]+$/, message: "Somente texto é permitido" },
+                        maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
+                      })}
+                    />
+                  </section>
+
+                  <section>
+                    <Select
+                      id="category"
+                      label="Categoria"
+                      value={plateDataForm.category}
+                      options={["Refeições", "Sobremesas", "Bebidas"]}
+                      onChange={handleInputChange}
+                      error={errors.category?.message}
+                      register={register("category", {
+                        required: plateDataForm.category ? false : "Campo obrigatório",
+                        pattern: { value: /^(Refeições|Sobremesas|Bebidas)$/, message: "Somente uma das opções é permitido" },
+                        maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
+                      })}
+                    />
+                  </section>
                 </div>
-                {errors.ingredients &&
-                  (errors.ingredients.root ?
-                    <span className='inputError'>{errors.ingredients.root.message}</span> :
-                    <span className='inputError'>{errors.ingredients.message}</span>
-                  )
-                }
-              </section>
 
-              <section>
-                <Input
-                  id="price"
-                  label="Preço"
-                  type="text"
-                  placeholder="R$ 0,00"
-                  value={plateDataForm.price}
-                  onChange={(event) => handleFormatPrice(event.target.value)}
-                  error={errors.price?.message}
-                  register={register("price", {
-                    required: plateDataForm.price ? false : "Campo obrigatório",
-                    pattern: { value: /^R\$\s[0-9]+([.][0-9]+)?([,][0-9]+)?$/, message: "Insira um valor válido. Ex: R$ 10,25" },
-                    maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
-                  })}
-                />
-              </section>
-            </div>
+                <div className="formPart2">
+                  <section className="containerIngredients">
+                    <label htmlFor="boxIngredients">Ingredientes</label>
+                    <div id="boxIngredients">
+                      {fields?.map((ingredient, index) => (
+                        <InputList
+                          key={ingredient.id}
+                          value={ingredient.name}
+                          onClick={() => remove(index)}
+                        />
+                      ))}
 
-            <section>
-              <Textarea
-                id="description"
-                label="Descrição"
-                value={plateDataForm.description}
-                onChange={handleInputChange}
-                placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
-                error={errors.description?.message}
-                register={register("description", {
-                  required: plateDataForm.description ? false : "Campo obrigatório",
-                  maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
-                })}
-              />
-            </section>
-          </form>
+                      <InputList
+                        isNew
+                        placeholder="Adicionar"
+                        value={newIngredient}
+                        onChange={(event) => setNewIngredient(event.target.value)}
+                        onClick={handleAddIngredient}
+                      />
+                    </div>
+                    {errors.ingredients &&
+                      (errors.ingredients.root ?
+                        <span className='inputError'>{errors.ingredients.root.message}</span> :
+                        <span className='inputError'>{errors.ingredients.message}</span>
+                      )
+                    }
+                  </section>
 
-          <div className="formButtons">
-            <ButtonDelete name="Excluir prato" onClick={onDeletePlate} />
-            <ButtonSave name="Salvar alterações" form="formEditPlate" />
-          </div>
+                  <section>
+                    <Input
+                      id="price"
+                      label="Preço"
+                      type="text"
+                      placeholder="R$ 0,00"
+                      value={plateDataForm.price}
+                      onChange={(event) => handleFormatPrice(event.target.value)}
+                      error={errors.price?.message}
+                      register={register("price", {
+                        required: plateDataForm.price ? false : "Campo obrigatório",
+                        pattern: { value: /^R\$\s[0-9]+([.][0-9]+)?([,][0-9]+)?$/, message: "Insira um valor válido. Ex: R$ 10,25" },
+                        maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
+                      })}
+                    />
+                  </section>
+                </div>
+
+                <section>
+                  <Textarea
+                    id="description"
+                    label="Descrição"
+                    value={plateDataForm.description}
+                    onChange={handleInputChange}
+                    placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+                    error={errors.description?.message}
+                    register={register("description", {
+                      required: plateDataForm.description ? false : "Campo obrigatório",
+                      maxLength: { value: 255, message: "Número máximo de caracteres é 255" }
+                    })}
+                  />
+                </section>
+              </form>
+
+              <div className="formButtons">
+                <ButtonDelete name="Excluir prato" onClick={onDeletePlate} />
+                <ButtonSave name="Salvar alterações" form="formEditPlate" />
+              </div>
+            </>
+          }
         </>
       }
     </Container>
