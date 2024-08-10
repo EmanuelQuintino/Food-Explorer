ab5185c1110ba1dd94a5bcc2d1c640d7a5fd99c3
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-08-10 11:57:22 -0300

    update format code

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
-import Theme from './styles/theme'
-import { GlobalStyles } from './styles/global'
-import { SystemProvider } from './hooks/useSystem'
-import { Routes } from "./routes"
-import { AuthProvider } from './hooks/useAuth'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import Theme from "./styles/theme";
+import { GlobalStyles } from "./styles/global";
+import { SystemProvider } from "./hooks/useSystem";
+import { Routes } from "./routes";
+import { AuthProvider } from "./hooks/useAuth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <SystemProvider>
@@ -30,5 +30,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         </AuthProvider>
       </SystemProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
